3a9d70b1f853786982830807fe17a43bb6a138db
Abhijeet Narsale <abhijeetnarsale14082004@gmail.com>
2026-07-21 11:34:09 +0530

    Update Cors Adding the frontend url in origin & Api Url for Deployement

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174","https://job-portal-silk-beta.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,12 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
+// export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
+// export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
+// export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+// export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
+
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const USER_API_END_POINT = `${API_URL}/user`;
+export const JOB_API_END_POINT = `${API_URL}/job`;
+export const COMPANY_API_END_POINT = `${API_URL}/company`;
+export const APPLICATION_API_END_POINT = `${API_URL}/application`;
